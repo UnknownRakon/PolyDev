3b4a13f764cb5dff4aec11bcd2626f5f9734a5b4
Vlad <gorenfeo@gmail.com>
2021-10-21 01:20:29 +0300

    Переписал Inctruction на FuncComp

--- src/panels/Instruction.js
+++ src/panels/Instruction.js
@@ -21,57 +21,50 @@ import { Icon28LightbulbStarOutline } from '@vkontakte/icons';
 import { Link } from '@vkontakte/vkui';
 
 import '../css/question.css'
+const Instruction = ({ id, category, question, go, back }) => {
 
-class Instruction extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            category: ''
-        }
-    }
-    createMarkup = (text) => {
+    const createMarkup = (text) => {
         return { __html: text };
     }
-    render() {
-        return (
-            <Panel id={this.props.id}>
-                <PanelHeader left={<PanelHeaderBack onClick={this.props.back} />}>PolyApp</PanelHeader>
-                <Div>
-                    {this.props.category == 'dorms' ? <Icon36HomeOutline className="instruction__icon" /> : null}
-                    {this.props.category == 'study' ? <Icon28BookOutline className="instruction__icon" /> : null}
-                    {this.props.category == 'buildings' ? <Icon56SchoolOutline className="instruction__icon" /> : null}
-                    {this.props.category == 'PD' ? <Icon28LightbulbStarOutline className="instruction__icon" /> : null}
-                    <Title style={{ marginTop: 10, textAlign: 'center' }} level="2" weight="medium">{this.props.question.question}</Title>
-                </Div>
-                <Group className="group-about" style={{ marginBottom: 130 }}>
-                    <Div style={{ paddingBlockEnd: 0 }}><Headline weight="regular" dangerouslySetInnerHTML={this.createMarkup(this.props.question.instruction)}></Headline></Div>
-                    {this.props.question.links.length !== 0 ? <Header className='heading' style={{ padding: 0 }} mode="secondary">Полезные ссылки</Header> : null}
-                    {this.props.question.links !== 0 &&
-                        this.props.question.links.map((link, index) => {
-                            return (
-                                <Div style={{ paddingBlockEnd: 0, paddingBlockStart: 0 }}><Link key={index} className='link-instruction' href={link.href} target="_blank">{link.name}</Link></Div>
-                            )
-                        }
+    return (
+        <Panel id={id}>
+            <PanelHeader left={<PanelHeaderBack onClick={back} />}>PolyApp</PanelHeader>
+            <Div>
+                {category == 'dorms' ? <Icon36HomeOutline className="instruction__icon" /> : null}
+                {category == 'study' ? <Icon28BookOutline className="instruction__icon" /> : null}
+                {category == 'buildings' ? <Icon56SchoolOutline className="instruction__icon" /> : null}
+                {category == 'PD' ? <Icon28LightbulbStarOutline className="instruction__icon" /> : null}
+                <Title style={{ marginTop: 10, textAlign: 'center' }} level="2" weight="medium">{question.question}</Title>
+            </Div>
+            <Group className="group-about" style={{ marginBottom: 130 }}>
+                <Div style={{ paddingBlockEnd: 0 }}><Headline weight="regular" dangerouslySetInnerHTML={createMarkup(question.instruction)}></Headline></Div>
+                {question.links.length !== 0 ? <Header className='heading' style={{ padding: 0 }} mode="secondary">Полезные ссылки</Header> : null}
+                {question.links !== 0 &&
+                    question.links.map((link, index) => {
+                        return (
+                            <Div style={{ paddingBlockEnd: 0, paddingBlockStart: 0 }}><Link key={index} className='link-instruction' href={link.href} target="_blank">{link.name}</Link></Div>
                         )
                     }
+                    )
+                }
 
-                </Group>
-                <FixedLayout filled vertical="bottom">
-                    <Tabbar className='tabbar-padding'>
-                        <TabbarItem selected text="Вопросы" onClick={this.props.go} data-to="questions">
-                            <Icon28InfoCircleOutline />
-                        </TabbarItem>
-                        <TabbarItem text="Календарь" onClick={this.props.go} data-to="calendar">
-                            <Icon28CalendarOutline />
-                        </TabbarItem>
-                        <TabbarItem text="Профиль" onClick={this.props.go} data-to="home">
-                            <Icon28UserCircleOutline />
-                        </TabbarItem>
-                    </Tabbar>
-                </FixedLayout>
-            </Panel>
-        )
-    }
+            </Group>
+            <FixedLayout filled vertical="bottom">
+                <Tabbar className='tabbar-padding'>
+                    <TabbarItem selected text="Вопросы" onClick={go} data-to="questions">
+                        <Icon28InfoCircleOutline />
+                    </TabbarItem>
+                    <TabbarItem text="Календарь" onClick={go} data-to="calendar">
+                        <Icon28CalendarOutline />
+                    </TabbarItem>
+                    <TabbarItem text="Профиль" onClick={go} data-to="home">
+                        <Icon28UserCircleOutline />
+                    </TabbarItem>
+                </Tabbar>
+            </FixedLayout>
+        </Panel>
+    )
+
 }
 
 export default Instruction;
